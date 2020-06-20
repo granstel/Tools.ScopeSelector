429f2ac14ff7cb10903130fd0b26464e411b31a9
Stepan Grankin <sgrankin@inbox.ru>
2020-06-20 21:24:27 +0300

    start

---
+++ src/Balancer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GranSteL.DialogflowBalancer
+{
+    public class Balancer
+    {
+        private readonly string[] _jsonKeysPathes;
+
+        private readonly MemoryCache _cache;
+
+        public Balancer(string[] jsonKeysPathes)
+        {
+            _jsonKeysPathes = jsonKeysPathes;
+
+            _cache = new MemoryCache(new MemoryCacheOptions());
+
+            InitClients();
+        }
+
+        private void InitClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        private TItem GetOrCreate<TItem>(object key, Func<TItem> createItem)
+        {
+            TItem cacheEntry;
+            if (!_cache.TryGetValue(key, out cacheEntry)) // Ищем ключ в кэше.
+            {
+                // Ключ отсутствует в кэше, поэтому получаем данные.
+                cacheEntry = createItem();
+
+                // Сохраняем данные в кэше. 
+                _cache.Set(key, cacheEntry);
+            }
+            return cacheEntry;
+        }
+    }
+}
